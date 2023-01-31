4ece5646e47bf8f81689638a450cdd925e601d1d
training70 <rabaninavyll@gmail.com>
2023-01-31 16:06:27 +0700

    first commit

---
+++ Javascript/page.js
@@ -0,0 +1,25 @@
+/*--- 
+
+Author: YasirKareem
+Channel: Design & Code
+Link: https://www.youtube.com/DesignCodeY
+Date: Feb 2, 2019
+If you like the lesson please subscribe to the channel
+
+---*/
+
+/*global $*/
+
+$(function () {
+    'use strict';
+    var view = $(window).height();
+    $('.part')
+        .height(view)
+        .scrollie({
+            scrollOffset: -50,
+            scrollingInView: function (elem) {
+                var bgColor = elem.data('background');
+                $('body').css('background-color', bgColor);
+            }
+        });
+});
---
+++ Javascript/redirect.js
@@ -0,0 +1,43 @@
+let timeMove = 0;
+let timeDown = '';
+neumorph($('.buttonWrap'));
+$('#reset').on('click', function () {
+    reset();
+});
+function neumorph(selector) {
+    selector.on('mousedown', function () {
+        selector.addClass('active');
+        timeDown = setInterval(timeUp, 100);
+    });
+    selector.on('touchstart', function () {
+        selector.addClass('active');
+        timeDown = setInterval(timeUp, 100);
+    });
+    selector.on('mouseup', function () {
+        selector.removeClass('active');
+        stopTime();
+    });
+    selector.on('touchend', function () {
+        selector.removeClass('active');
+        stopTime();
+    });
+    $('button').on('mouseleave', function () {
+        selector.removeClass('active');
+        stopTime();
+    });
+}
+function timeUp() {
+    if (timeMove >= 110) {
+        clearInterval(timeDown);
+        $('body').addClass('done');
+        console.log('stop');
+        location.replace('mmi-bab3-1.html');
+    } else {
+        timeMove = timeMove + 2;
+        $('.progress').css('transform', 'translateY(calc(100% + 111px - ' + timeMove + '%))');
+        console.log(timeMove);
+    }
+}
+function stopTime() {
+    clearInterval(timeDown);
+}
